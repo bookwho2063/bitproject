8fe3dd65de85d0eadf062441622ed9824c6e3ede
bookwho2063 <junuk103@gmail.com>
2019-10-15 12:20:27 +0900

    vggfacerealtime

---
+++ 02.Source/dev/BtiProject/vggFaceRealTime.py
@@ -0,0 +1,395 @@
+import cv2
+import os, glob, pickle, platform, site
+import numpy as np
+from time import sleep
+from keras.engine import Model
+from keras import layers, models
+from keras.layers import Input
+from keras_vggface.vggface import VGGFace
+from keras_preprocessing import image
+from keras_applications.resnet50 import ResNet50
+from keras_applications.resnet50 import preprocess_input
+from keras_vggface import utils
+from scipy.spatial import distance as scipyDistance
+
+
+def load_stuff(filename):
+    """
+    pickle file load
+    :param filename:
+    :return:
+    """
+    saved_stuff = open(filename, "rb")
+    stuff = pickle.load(saved_stuff)
+    saved_stuff.close()
+    print("=====loaded stuff success")
+    return stuff
+
+def pickleStuff(fileName, stuff):
+    """
+    pickle file generator
+    :param fileName:
+    :param stuff:
+    :return:
+    """
+    saveStuff = open(fileName, "wb")
+    pickle.dump(stuff, saveStuff)
+    saveStuff.close()
+    print("=====create stuff success")
+
+class vggFaceExtractor(object):
+
+    def __new__(cls, weight_file=None, face_size=224):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(vggFaceExtractor, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self, face_size=224):
+        self.FACE_IMAGES_FOLDER = "../00.Resource/data/faceImages"
+        self.VIDEOS_FOLDER = "../00.Resource/data/videos"
+        self.resnet50Features = None
+        self.osName = ""            # OS명
+        self.face_cascade = ""      # cascade 타겟변수
+        self.faceSize = face_size   # 얼굴 검출 사진 사이즈
+        self.batchSize = 16         # 배치사이즈
+        self.className = None       # 사용자가 입력한 클래스명
+        self.saveFolder = None      # vggfaceInit() 을 통해 생성되는 클래스폴더경로
+        self.savePkPath = "../00.Resource/data/pickle/"  # 피클파일 저장경로
+        self.savePkNm = "precompute_features_40000_bat2.pickle"         # 피클파일 저장파일명
+
+
+
+    def createPickle(self):
+        """
+        pickle 파일을 생성한다.
+        :return: 저장 성공여부
+        """
+        pickleDataList = []
+        faceFolder = self.FACE_IMAGES_FOLDER
+        className = self.className
+        pickleFilePath = os.path.join(self.savePkPath, self.savePkNm)
+        mean_features = self.calMeanFeature(imageFolder=os.path.join(faceFolder, className))
+        pickleDataList.append({"name": className, "features": mean_features})
+        pickleStuff(pickleFilePath, pickleDataList)
+
+        if os.path.isfile(pickleFilePath):
+            return True
+        else:
+            return False
+
+
+    def loadPickle(self, loadPath):
+        """
+        pickle 파일을 로드 한다
+        :param loadPath: 로드할 대상 파일 경로
+        :return: 로드 클래스명 (썸네일 생성을 위함)
+        """
+        pass
+        return "loadClassList"
+
+    def osSetting(self):
+        """
+        OS Platform 에 따른 분류
+        :return:
+        """
+        if platform.system() == "Windows":
+            self.osName = "Windows"
+            self.face_cascade = cv2.CascadeClassifier(
+                os.path.join(site.getsitepackages()[1], "cv2/data/haarcascade_frontalface_default.xml"))
+        elif platform.system() == "Linux":
+            self.osName = "Linux"
+            self.face_cascade = cv2.CascadeClassifier(
+                os.path.join(site.getsitepackages()[0], "cv2/data/haarcascade_frontalface_default.xml"))
+
+
+    def cropFace(self, imgarray, section, margin=20, size=224):
+        """
+        프레임을 crop 하여 결과를 리턴한다.
+        :param imgarry: full Image
+        :param section: face Detected Area (x, y, w, h)
+        :param margin: add some margin to the face detected area
+        :param size: result image resolution
+        :return: resize image (size * size * 3)
+        """
+
+        img_h, img_w, _ = imgarray.shape
+
+        if section is None:
+            section = [0, 0, img_w, img_h]
+
+        (x, y, w, h) = section
+
+        margin = int(min(w,h) * margin / 100)
+        x_a = x - margin
+        y_a = y - margin
+        x_b = x + w + margin
+        y_b = y + h + margin
+
+        if x_a < 0:
+            x_b = min(x_b - x_a, img_w-1)
+            x_a = 0
+        if y_a < 0:
+            y_b = min(y_b - y_a, img_h - 1)
+            y_a = 0
+        if x_b > img_w:
+            x_a = max(x_a - (x_b - img_w), 0)
+            x_b = img_w
+        if y_b > img_h:
+            y_a = max(y_a - (y_b - img_h), 0)
+            y_b = img_h
+
+        # print("after if :: {}, {}, {}, {}".format(x_a, y_a, x_b, y_b))
+
+        cropped = imgarray[y_a: y_b, x_a: x_b]
+        resized_img = cv2.resize(cropped, (size, size), interpolation=cv2.INTER_AREA)
+        resized_img = np.array(resized_img)
+        return resized_img, (x_a, y_a, x_b - x_a, y_b - y_a)
+
+    def extractFace(self, frame, frameNum):
+        """
+        프레임에서 얼굴을 검출하고 저장한다.
+        :param frame: 프레임 정보를 인자로 준다
+        :return:
+        """
+        # OS에 따라 처리분기
+        self.osSetting()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=10,
+            minSize=(64, 64)
+        )
+
+        # only keep the biggest face as the main subject
+        face = None
+        if len(faces) > 1:  # Get the largest face as main face
+            face = max(faces, key=lambda rectangle: (rectangle[2] * rectangle[3]))  # area = w * h
+        elif len(faces) == 1:
+            face = faces[0]
+
+        if face is not None:
+            face_img, cropped = self.cropFace(frame, face, margin=40, size=self.faceSize)
+            (x, y, w, h) = cropped
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 200, 0), 2)
+            # cv2.imshow('Faces', frame)
+
+            # 이미지 데이터 특정경로 저장
+            imgfile = os.path.basename(self.className) + str(frameNum) + ".png"
+            imgfile = os.path.join(str(self.saveFolder), str(imgfile))
+            print("imgfile :: ", imgfile)
+            cv2.imwrite(imgfile, face_img)
+
+    def image2x(self, imagePath):
+        """
+        이미지의 평균치를 구한다.
+        :param imagePath:
+        :return:
+        """
+        img = image.load_img(imagePath, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = utils.preprocess_input(x, version=1) # or version = 2
+        return x
+
+    def calMeanFeature(self, imageFolder):
+        faceImages = list(glob.iglob(os.path.join(imageFolder, "*")))
+
+        def chunks(l, n):
+            """
+            batchSize 만큼의 제너레이터를 생성하여 리턴한다.
+            :param l:
+            :param n:
+            :return:
+            """
+            for i in range(0, len(l), n):
+                yield l[i:i+n]
+
+        faceImageChunks = chunks(faceImages, self.batchSize)
+        fvecs = None
+
+        for faceImageChunk in faceImageChunks:
+            images = np.concatenate([self.image2x(faceImage) for faceImage in faceImageChunk])
+            batchFvecs = self.resnet50Features.predict(images)
+
+            if fvecs is None:
+                fvecs = batchFvecs
+            else:
+                fvecs = np.append(fvecs, batchFvecs, axis=0)
+
+            print("========== 생성중 ... [ " + str(len(fvecs)) + " ]")
+        return np.array(fvecs).sum(axis=0) / len(fvecs)
+
+    def vggfaceInit(self):
+        """
+        초기화 작업
+        :return:
+        """
+        if self.resnet50Features is None:
+            self.resnet50Features = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+        if self.FACE_IMAGES_FOLDER is None:
+            self.FACE_IMAGES_FOLDER = "../00.Resource/data/faceImages"
+        if self.VIDEOS_FOLDER is None:
+            self.VIDEOS_FOLDER = "../00.Resource/data/videos"
+        if self.className is None:
+            self.className = "className"
+
+        # 영상이 저장될 클래스 네임폴더 생성
+        targetFolder = os.path.join(self.FACE_IMAGES_FOLDER, self.className)
+        if os.path.isdir(targetFolder):
+            print("===== folder exist!!!! :: ", targetFolder)
+        else:
+            print("===== folder create :: ", targetFolder)
+            os.makedirs(targetFolder, exist_ok=True)
+            self.saveFolder = targetFolder
+
+
+class recognitionFace(object):
+    """
+    INFO : VGGFACE2를 이용하여 영상 내 프레임에서 얼굴을 검출하는 클래스
+    """
+    def __new__(cls, precompute_features_file=None):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(recognitionFace, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self, precompute_features_file=None):
+        self.face_size = 224
+        self.precompute_features_map = load_stuff(precompute_features_file)
+        self.face_cascade = None
+        print("Loading VGG Face model...")
+        self.model = None
+        print("Loading VGG Face model done")
+
+    def vggRecogInit(self):
+        """
+        class init method
+        :return:
+        """
+        if self.model == None:
+                self.model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')  # pooling: None, avg or max
+
+
+    @classmethod
+    def draw_label(cls, image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
+                   font_scale=1, thickness=2):
+        # cv2
+        size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+        x, y = point
+        cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
+        cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness)
+
+    def crop_face(self, imgarray, section, margin=20, size=224):
+        """
+        :param imgarray: full image
+        :param section: face detected area (x, y, w, h)
+        :param margin: add some margin to the face detected area to include a full head
+        :param size: the result image resolution with be (size x size)
+        :return: resized image in numpy array with shape (size x size x 3)
+        """
+        img_h, img_w, _ = imgarray.shape
+        if section is None:
+            section = [0, 0, img_w, img_h]
+        (x, y, w, h) = section
+        margin = int(min(w, h) * margin / 100)
+        x_a = x - margin
+        y_a = y - margin
+        x_b = x + w + margin
+        y_b = y + h + margin
+        if x_a < 0:
+            x_b = min(x_b - x_a, img_w - 1)
+            x_a = 0
+        if y_a < 0:
+            y_b = min(y_b - y_a, img_h - 1)
+            y_a = 0
+        if x_b > img_w:
+            x_a = max(x_a - (x_b - img_w), 0)
+            x_b = img_w
+        if y_b > img_h:
+            y_a = max(y_a - (y_b - img_h), 0)
+            y_b = img_h
+        cropped = imgarray[y_a: y_b, x_a: x_b]
+        resized_img = cv2.resize(cropped, (size, size), interpolation=cv2.INTER_AREA)
+        resized_img = np.array(resized_img)
+        return resized_img, (x_a, y_a, x_b - x_a, y_b - y_a)
+
+    def identify_face(self, features, threshold=100):
+
+        # name 변경용 dict
+        nameChDict = dict()
+        nameChDict['로제'] = 'Rose'
+        nameChDict['제니'] = 'Jennie'
+        nameChDict['리사'] = 'Lisa'
+        nameChDict['지수'] = 'Jisoo'
+
+        distances = []
+        for person in self.precompute_features_map:
+            person_features = person.get("features")
+            distance = scipyDistance.euclidean(person_features, features)
+            distances.append(distance)
+        min_distance_value = min(distances)
+        min_distance_index = distances.index(min_distance_value)
+        print("min_distance_value :: ", min_distance_value)
+        print("self.precompute_features_map[min_distance_index].get(name) :: ",
+              self.precompute_features_map[min_distance_index].get("name"))
+        if min_distance_value < threshold:
+            # return str(self.precompute_features_map[min_distance_index].get("name")), str(min_distance_value)
+            return nameChDict[str(self.precompute_features_map[min_distance_index].get("name"))], str(min_distance_value)
+        else:
+            return "???", "???"
+
+    def osSetting(self):
+        if platform.system() == "Windows":
+            self.face_cascade = cv2.CascadeClassifier(os.path.join(site.getsitepackages()[1], "cv2/data/haarcascade_frontalface_default.xml"))
+        elif platform.system() == "Linux":
+            self.face_cascade = cv2.CascadeClassifier(os.path.join(site.getsitepackages()[0], "cv2/data/haarcascade_frontalface_default.xml"))
+
+    def detect_face(self, frame):
+        """
+        프레임데이터 입력 시 검출 결과 정보를 리턴한다.
+        :param frame:
+        :return:
+        """
+
+        self.osSetting()
+        predicted_names = list()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=10, minSize=(64, 64))
+
+        # placeholder for cropped faces
+        face_imgs = np.empty((len(faces), self.face_size, self.face_size, 3))
+
+        tempCoordList = list()
+        for i, face in enumerate(faces):
+            face_img, cropped = self.crop_face(frame, face, margin=10, size=self.face_size)
+            (x, y, w, h) = cropped
+            # 검출 결과 좌표 데이터 설정
+            tempCoordDict = dict()
+            tempCoordDict['x'] = x
+            tempCoordDict['y'] = y
+            tempCoordDict['w'] = w
+            tempCoordDict['h'] = h
+            tempCoordList.append(tempCoordDict)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 200, 0), 2)
+            face_imgs[i, :, :, :] = face_img
+
+        # 페이스 데이터 검증
+        if len(face_imgs) > 0:
+            features_faces = self.model.predict(face_imgs)
+            predicted_names = [self.identify_face(features_face) for features_face in features_faces]
+
+            # 데이터 메타데이터셋 정립
+            for idx in range(len(predicted_names)):
+                (cName, cPer) = predicted_names[idx]
+                tempCoordList[idx]['labelname'] = str(cName)
+                tempCoordList[idx]['percent'] = str(round(float(cPer),2))
+        else:
+            print("=========== len(face_imgs) < 0")
+
+        # draw results
+        for i, face in enumerate(faces):
+            label = "{}".format(predicted_names[i])
+            self.draw_label(frame, (face[0], face[1]), label)
+
+        return frame, tempCoordList
